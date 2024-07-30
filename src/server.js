d01959faae62c4958217463372b6cac2c8f88f93
Halal Kharbouch <binyaminbgod@gmail.com>
2024-07-30 16:56:05 +0100

    optimized server.js

--- src/server.js
+++ src/server.js
@@ -5,15 +5,19 @@ import { dirname } from "path";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+
+// Importing routes
 import homeRouter from "./routes/home.route.js";
 import aboutRouter from "./routes/about.route.js";
 import contactRouter from "./routes/contact.route.js";
 import servicesRouter from "./routes/services.route.js";
 import portfoliosRouter from "./routes/portfolios.route.js";
 import blogRouter from "./routes/blog.route.js";
-import { formatDateUtil, trimText } from "./utils/utils.js";
 import mailRouter from "./routes/mail.route.js";
 import authRouter from "./routes/auth.router.js";
+
+// Importing utilities
+import { formatDateUtil, trimText } from "./utils/utils.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -21,73 +25,45 @@ const __dirname = dirname(__filename);
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Make formatDateUtil available to all templates
+// Make utilities available to all templates
 app.locals.formatDateUtil = formatDateUtil;
 app.locals.trimText = trimText;
 
-//Middleware to parse json and URL-encoded request bodies
+// Middleware to parse JSON and URL-encoded request bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Set a larger timeout value (e.g., 1 minutes)
-// app.use((req, res, next) => {
-//   req.setTimeout(1 * 60 * 1000); // 1 minutes in milliseconds
-//   res.setTimeout(1 * 60 * 1000);
-//   next();
-// });
-
-// Debugging middleware to log requests
-// app.use((req, res, next) => {
-//   console.log("Request URL:", req.originalUrl);
-//   console.log("Request Method:", req.method);
-//   console.log("Request Headers:", req.headers);
-//   console.log("Request Body:", req.body);
-//   next();
-// });
-
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_ATLAS_URI)
   .then(() => console.log("Connected to DB"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.error("DB connection error:", err));
 
-//set up EJS as the template engine
+// Set up EJS as the template engine
 app.set("view engine", "ejs");
 
-//serve static files froom public directory
+// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, "public")));
 
-//set up views directory
+// Set up views directory
 app.set("views", path.join(__dirname, "views"));
 
-//root route
+// Routes
 app.use("/", homeRouter);
-
-//about route
 app.use("/about", aboutRouter);
-
-//services route
 app.use("/services", servicesRouter);
-
-//portfolios route
 app.use("/portfolios", portfoliosRouter);
-
-//blog route
 app.use("/blog", blogRouter);
-
-//contact route
 app.use("/contact", contactRouter);
-
-//mail route
 app.use("/send-mail", mailRouter);
-
-//auth route
 app.use("/auth", authRouter);
 
-//blog-detail route
-app.route("/blog-details").get((req, res) => {
+// Blog details route
+app.get("/blog-details", (req, res) => {
   res.render("blog-details");
 });
 
-app.listen(PORT, () => console.log("Server is running on port 3000"));
+// Start server
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
